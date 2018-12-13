769b2f50503c08fbe7ea252096fb718ed3f3f724
pawelsoundso <pawel.gloeckner@student.hpi.uni-potsdam.de>
2018-12-13 14:05:09 +0100

    Add conversion into Tuples

--- src/main/scala/de/hpi/spark_tutorial/Sindy.scala
+++ src/main/scala/de/hpi/spark_tutorial/Sindy.scala
@@ -14,10 +14,20 @@ object Sindy {
 
     //datasets.flatMap(row => row.columns.map(
     //  columnName => (columnName, row.getAs(columnName))
-    //))
-    val temp = datasets.flatMap(row => row.columns)
-    println(temp)
 
+    /*val temp = datasets.head.flatMap(row => {
+      val dict = row.getValuesMap(bcNames.value(0))
+      dict.toList.map( e => (e._1.toString, e._2.toString))
+    })*/
+    import spark.implicits._
+    val bcNames = spark.sparkContext.broadcast(datasets.map(dataset => dataset.columns))
+    //))
+    bcNames.value.foreach(_.foreach(println))
+    val temp = datasets.head.flatMap(row => bcNames.value(0).map( column => {
+      (column.toString, row.getAs(column).toString)
+    }))
+    temp.show()
+    datasets.head.show()
 
 
   }
